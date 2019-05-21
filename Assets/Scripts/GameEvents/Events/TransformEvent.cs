dc40e98a2946bd4de050ff727bb44ab8119726e2
PC_Portable\axel <axeldeconti@gmail.com>
2019-05-21 15:24:05 +0200

    Game Event System (Void, Int, Float, Vector3, Transform)

---
+++ Assets/Scripts/GameEvents/Events/TransformEvent.cs
@@ -0,0 +1,4 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Transform Event", menuName = "Game Events/Transform Event")]
+public class TransformEvent : BaseGameEvent<Transform> { }
---
+++ Assets/Scripts/GameEvents/Listeners/TransformListener.cs
@@ -0,0 +1,3 @@
+﻿using UnityEngine;
+
+public class TransformListener : BaseGameEventListener<Transform, TransformEvent, UnityTransformEvent> { }
