b58cd238b354631bf6f1e712164f8b1660f019e9
Axel <axeldeconti@gmail.com>
2019-05-27 10:01:34 +0200

    FloatVariable et demo

---
+++ Assets/TestVariables/Normal/PlayerHP.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHP : MonoBehaviour
+{
+    public float maxHp = 100, hp = 100;
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            hp -= 10;
+            UIManager.instance.SetHealthSliderValue(hp / maxHp);
+        }
+            
+    }
+}
---
+++ Assets/TestVariables/Variable/PlayerHPVariable.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHPVariable : MonoBehaviour
+{
+    public FloatReference maxHP;
+    public FloatVariable hp;
+
+    private void Start()
+    {
+        hp.SetValue(maxHP);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            hp.ApplyChange(-10);
+        }
+    }
+}
