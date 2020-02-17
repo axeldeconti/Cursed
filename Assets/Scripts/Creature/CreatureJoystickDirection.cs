67b8cf92be1fc525fa8a940cf09d2efe60b159c0
Théo Gautier <36964591+WarZicks@users.noreply.github.com>
2020-02-17 15:33:35 +0100

    [Dev] Creature joystick clamp direction + creature anim to wall faster

--- Assets/Scripts/Creature/CreatureJoystickDirection.cs
+++ Assets/Scripts/Creature/CreatureJoystickDirection.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using UnityEngine;
+using Cursed.Character;
 
 namespace Cursed.Creature
 {
@@ -15,11 +16,13 @@ namespace Cursed.Creature
         private Vector2 _direction;
         private CreatureManager _creature;
         private CreatureInputController _input;
+        private CollisionHandler _playerCollision;
 
         private void Awake()
         {
             _creature = GetComponent<CreatureManager>();
             _input = GetComponent<CreatureInputController>();
+            _playerCollision = GameObject.FindGameObjectWithTag("Player").GetComponent<CollisionHandler>();
         }
 
         private void Update()
@@ -28,7 +31,18 @@ namespace Cursed.Creature
             {
                 float mag = Mathf.Clamp01(new Vector2(Input.GetAxis("HorizontalRight"), Input.GetAxis("VerticalRight")).magnitude);
                 if (mag > .9f)
+                {
                     _direction = Vector2.right * Input.GetAxisRaw("HorizontalRight") + Vector2.up * Input.GetAxisRaw("VerticalRight");
+                    float angle = Mathf.Atan2(_direction.y, _direction.x) * Mathf.Rad2Deg;
+
+                    if (_playerCollision.OnGround)
+                    {
+                        if (angle < -90f)
+                            _direction = -Vector2.right;
+                        else if (angle < 0f && angle >= -90f)
+                            _direction = Vector2.right;
+                    }
+                }
 
                 if (_direction != Vector2.zero && _target == null && _input.Holding && !_creature.Recall)
                 {
