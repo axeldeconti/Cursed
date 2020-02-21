070a86a360fbf209375fac6dee45ebdc0aee9210
Théo Gautier <36964591+WarZicks@users.noreply.github.com>
2020-02-21 11:18:40 +0100

    [Dev] Laser beam particles + laser holder sprite int

---
+++ Assets/Scripts/Traps/ParticleLaserSize.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace Cursed.Traps
+{
+    public class ParticleLaserSize : MonoBehaviour
+    {
+        private ParticleSystem _particles;
+        [SerializeField] private Transform _end;
+
+        private void Awake()
+        {
+            _particles = GetComponent<ParticleSystem>();
+        }
+
+        private void Start()
+        {
+            _particles.startLifetime = (_end.localPosition.y) / 100; 
+        }
+    }
+}
