74b41ee69442835d40a4b74dd631e6ba0876f973
TomRivaud <temtonik@gmail.com>
2020-03-02 10:19:27 +0100

    [Dev] SFX Handler

--- Assets/Scripts/SFX/SFXHandler.cs
+++ Assets/Scripts/SFX/SFXHandler.cs
@@ -1,24 +1,18 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using Cursed.Character;
 
 public class SFXHandler : MonoBehaviour
 {
     private Animator _anim = null;
-    private CharacterMovement _move = null;
 
     private void Start()
     {
         _anim = GetComponent<Animator>();
-        _move = GetComponentInParent<CharacterMovement>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (_move.IsDashing)
-            AkSoundEngine.PostEvent("Play_Dash", gameObject);
 
     }
 }
