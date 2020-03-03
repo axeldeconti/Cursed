1c6ff73d9c76a9b4bc560505273495f40d601a2b
TomRivaud <temtonik@gmail.com>
2020-03-03 17:39:25 +0100

    [Dev] Changes applied to prefab + Update Soundbank

--- Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -13,14 +13,20 @@ namespace AK
 {
     namespace EVENTS
     {
+        static const AkUniqueID PLAY_ATTACK_AXE_FIRSTSLICE = 1312214966U;
+        static const AkUniqueID PLAY_ATTACK_SWORD_FIRSTSLICE = 2804287435U;
         static const AkUniqueID PLAY_BASICJUMP = 2216073360U;
         static const AkUniqueID PLAY_CREATURE_CALL = 1776683280U;
         static const AkUniqueID PLAY_CREATURE_GRABBING = 3373431376U;
         static const AkUniqueID PLAY_CREATURE_HITWALL = 1251478039U;
         static const AkUniqueID PLAY_CREATURE_LAUNCH = 3361186193U;
         static const AkUniqueID PLAY_DASH = 2211787386U;
+        static const AkUniqueID PLAY_DOUBLEJUMP = 595748461U;
+        static const AkUniqueID PLAY_END_WALLSLIDE = 2912867303U;
         static const AkUniqueID PLAY_LANDING = 2323405115U;
+        static const AkUniqueID PLAY_MAIN_WALLSLIDE = 3033462115U;
         static const AkUniqueID PLAY_RUN = 2822207957U;
+        static const AkUniqueID STOP_MAIN_PLAY_LOOP_WALLSLIDE = 3156285719U;
     } // namespace EVENTS
 
     namespace BANKS
