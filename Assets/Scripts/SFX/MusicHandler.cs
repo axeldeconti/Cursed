9090e405b697c83eb73449b5629f3b619486773f
TomRivaud <temtonik@gmail.com>
2020-03-10 10:46:59 +0100

    [Int] Music

---
+++ Assets/Scripts/SFX/MusicHandler.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class MusicHandler : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        AkSoundEngine.PostEvent("Play_Music", gameObject);
+    }
+}
