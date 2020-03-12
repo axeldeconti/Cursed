3effd8eda73964040c4e2da0d1040ffb3056316f
Zookan <36925604+Zookan@users.noreply.github.com>
2020-03-12 15:11:11 +0100

    [Dev] destructible box + remove old pikes prefabs

---
+++ Assets/Scripts/LevelBrics/DestructibleBox.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cursed.Combat;
+using Cursed.Character;
+
+namespace Cursed.Combat
+{
+    public class DestructibleBox : MonoBehaviour, IAttackable
+    {
+        public void OnAttack(GameObject attacker, Attack attack)
+        {
+            if(attacker.GetComponent<CharacterMovement>().IsDiveKicking)
+                Destroy(this.gameObject);
+        }
+    }
+}
