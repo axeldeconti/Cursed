603eaeeb4ba65b60a76b92570997dd31e3cc240d
Théo Gautier <36964591+WarZicks@users.noreply.github.com>
2020-03-18 16:36:15 +0100

    [Dev] Fix creature sonar rotation

--- Assets/Scripts/Creature/SonarEnemyDetection.cs
+++ Assets/Scripts/Creature/SonarEnemyDetection.cs
@@ -71,8 +71,11 @@ namespace Cursed.Creature
         {
             if (_targetDirection != null)
             {
-                float angle = UtilsClass.GetAngleFromVectorFloat(_targetDirection);
-                transform.localEulerAngles = new Vector3(0, 0, angle);
+                /*float angle = UtilsClass.GetAngleFromVectorFloat(_targetDirection);
+                transform.localEulerAngles = new Vector3(0, 0, angle);*/
+                float angle = Mathf.Atan2(_targetDirection.y, _targetDirection.x) * Mathf.Rad2Deg;
+                Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+                transform.rotation = Quaternion.Euler(rotation.eulerAngles);
             }
         }
 
