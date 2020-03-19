4b3a22285852c21fe330bb7e4c0d4e0357c3ccd4
Théo Gautier <36964591+WarZicks@users.noreply.github.com>
2020-03-19 16:14:36 +0100

    [Int] Destructible wall graph int + wall destruction effect

--- Assets/Scripts/LevelBrics/DestructibleBox.cs
+++ Assets/Scripts/LevelBrics/DestructibleBox.cs
@@ -8,9 +8,22 @@ namespace Cursed.Combat
 {
     public class DestructibleBox : MonoBehaviour, IAttackable
     {
+        [SerializeField] private GameObject _destructionEffect;
+
         public void OnAttack(GameObject attacker, Attack attack)
         {
+            CreateDestroyEffect(attacker.transform);
             Destroy(this.gameObject);
+        }
+
+        private GameObject CreateDestroyEffect(Transform attacker)
+        {
+            GameObject go = Instantiate(_destructionEffect, this.transform.position, Quaternion.identity);
+            Vector2 direction = this.transform.position - attacker.GetChild(0).position;
+            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+            Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+            go.transform.rotation = Quaternion.Euler(rotation.eulerAngles);
+            return go;
         }
     }
 }
