f528a33da70f3933dd3fe46c5b58a3a6e49aeb33
TomRivaud <temtonik@gmail.com>
2020-03-24 17:18:57 +0100

    [Dev] Monitor's numbers decrease

---
+++ Assets/Scripts/Props/TextChanger.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class TextChanger : MonoBehaviour
+{
+    private TextMeshProUGUI remainingAndroid;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        remainingAndroid = GetComponent<TextMeshProUGUI>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        remainingAndroid.text = GameObject.FindGameObjectsWithTag("Enemy").Length.ToString();
+    }
+}
