d5e571cbd11f83fcf1844b2532745eda6f84b73b
TomRivaud <temtonik@gmail.com>
2020-03-24 14:09:48 +0100

    [Dev] Moving laser

---
+++ Assets/LaserPath.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class LaserPath : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    public Transform[] moveSpots;
+    private int index;
+
+        // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, moveSpots[index].position, speed * Time.deltaTime);
+        if (transform.position.x == moveSpots[index].position.x)
+        {
+            if (index >= 1)
+                index = 0;
+            else
+                index++;
+        }
+            }
+}
