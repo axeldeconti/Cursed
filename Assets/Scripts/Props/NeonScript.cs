b4652ba09889741fbfd8d6274945c3add321f59e
TomRivaud <temtonik@gmail.com>
2020-03-25 17:17:41 +0100

    [Dev] Neon 1st flickering

---
+++ Assets/Scripts/Props/NeonScript.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NeonScript : MonoBehaviour
+{
+    public Animation flickeringNeon;
+
+    void Start()
+    {
+        flickeringNeon = GetComponent<Animation>();
+    }
+}
