db2a23994188964f4a421af4256072bcfd105c8f
Théo Gautier <36964591+WarZicks@users.noreply.github.com>
2020-03-27 12:26:26 +0100

    [Int] Tiles test room int v0.1

---
+++ Assets/Scripts/Utilities/FollowCamera.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using Cinemachine;
+
+namespace Cursed.Utilities
+{
+    public class FollowCamera : MonoBehaviour
+    {
+        [SerializeField] private CinemachineVirtualCamera _camera;
+
+        private void Update()
+        {
+            transform.position = new Vector3(_camera.transform.position.x, _camera.transform.position.y, 0);
+        }
+    }
+}
