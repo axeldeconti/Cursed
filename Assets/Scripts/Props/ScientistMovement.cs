9bfcf8465c5de55b905ed2a3c9bba4d2cf5eea8d
Théo Gautier <36964591+WarZicks@users.noreply.github.com>
2020-04-20 14:25:08 +0200

    [Dev + Int] Scientist background in main menu

---
+++ Assets/Scripts/Props/ScientistMovement.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class ScientistMovement : MonoBehaviour
+{
+    [HideInInspector] public Transform target;
+    [HideInInspector] public int flip;
+    private float _speed;
+
+
+    private void Start()
+    {
+        _speed = Random.Range(2f, 5f);
+        transform.localScale = new Vector2(flip * transform.localScale.x, transform.localScale.y);
+    }
+
+    private void Update()
+    {
+        transform.position = Vector2.MoveTowards(this.transform.position, target.position, _speed * Time.deltaTime);
+        CheckDistance();
+    }
+
+    private void CheckDistance()
+    {
+        if (transform.position.x == target.position.x)
+            Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/Props/ScientistSpawnerMenu.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScientistSpawnerMenu : MonoBehaviour
+{
+    [SerializeField] private bool _flip;
+    [SerializeField] private Transform _target;
+    [SerializeField] private GameObject _scientist;
+
+    private void Start()
+    {
+        StartCoroutine(WaitForSpawnScientist(0f));
+    }
+
+    private void SpawnScientist()
+    {
+        ScientistMovement scientist = Instantiate(_scientist, this.transform.position, Quaternion.identity, transform).GetComponent<ScientistMovement>();
+        scientist.target = _target;
+
+        if (_flip)
+            scientist.flip = -1;
+        else
+            scientist.flip = 1;
+    }
+
+    IEnumerator WaitForSpawnScientist(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        SpawnScientist();
+        StartCoroutine(WaitForSpawnScientist(Random.Range(1.5f, 4f)));
+    }
+}
