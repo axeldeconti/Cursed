c33506f6a6c4144adc9116e82882810c0dbfca55
Théo Gautier <36964591+WarZicks@users.noreply.github.com>
2020-04-22 10:37:29 +0200

    [Dev] Chromactic aberration changing depending on creature state

--- Assets/Scripts/Creature/CreatureManager.cs
+++ Assets/Scripts/Creature/CreatureManager.cs
@@ -210,6 +210,7 @@ namespace Cursed.Creature
                 {
                     case CreatureState.Moving:
                         //ToggleChilds(true);
+                        _vfx.CreatureAberration(_vfx._lowerChromacticAberrationValue);
                         _animator.SetBool("GoToCharacter", false);
                         _animator.SetBool("OnWall", false);
                         _animator.SetBool("Moving", true);
@@ -221,6 +222,7 @@ namespace Cursed.Creature
                     case CreatureState.OnCharacter:
                         //ToggleChilds(false);
                         _vfx.CreatureTouchImpactParticle(_collision.HitTransform.GetChild(0));
+                        _vfx.CreatureAberration(_vfx._higherChromacticAberrationValue);
                         _animator.SetBool("GoToCharacter", true);
                         _animator.SetBool("OnWall", false);
                         _animator.SetBool("Moving", false);
---
+++ Assets/Scripts/PostProcess/ChromaticAberrationChanging.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+
+namespace Cursed.PostProcess
+{
+    public class ChromaticAberrationChanging : MonoBehaviour
+    {
+        [SerializeField] private float _lerpSpeed = 5f;
+
+        private Volume _volume;
+        private ChromaticAberration _chromaticAberration;
+
+        private bool _launchLerp;
+        private float _targetAberration;
+
+        private void Awake()
+        {
+            _volume = GetComponent<Volume>();
+        }
+
+        private void Start()
+        {
+            ChromaticAberration chromaticAberration;
+
+            if (_volume.profile.TryGet<ChromaticAberration>(out chromaticAberration))
+                _chromaticAberration = chromaticAberration;
+        }
+
+        private void Update()
+        {
+            if (_launchLerp)
+                UpdateValue();
+        }
+
+        public void LerpToValue(float target)
+        {
+            _targetAberration = target;
+            _launchLerp = true;
+        }
+
+        private void UpdateValue()
+        {
+            _chromaticAberration.intensity.value = Mathf.Lerp(_chromaticAberration.intensity.value, _targetAberration, _lerpSpeed * Time.deltaTime);
+
+            if (_chromaticAberration.intensity.value == _targetAberration)
+                _launchLerp = false;
+        }
+    }
+}
