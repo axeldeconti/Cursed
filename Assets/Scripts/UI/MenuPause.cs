77a33356074112eff4c6fbf9649759898f48dc24
Théo Gautier <36964591+WarZicks@users.noreply.github.com>
2020-04-24 15:28:03 +0200

    [Int] Pause menu rework

--- Assets/Scripts/UI/MenuPause.cs
+++ Assets/Scripts/UI/MenuPause.cs
@@ -1,4 +1,6 @@
 ﻿using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
 
 namespace Cursed.UI
 {
@@ -6,14 +8,25 @@ namespace Cursed.UI
     {
         private GameManager _gameManager = null;
 
+        [Header("Referencies")]
         [SerializeField] private GameObject _pauseMenu = null;
         [SerializeField] private GameObject _controlsMenu = null;
+        [SerializeField] private GameObject _inGameMenu = null;
+
+        [Header("Post Process")]
+        [SerializeField] private Volume _globalVolume = null;
+        private DepthOfField _depthOfField;
 
         private void Start()
         {
             _gameManager = GameManager.Instance;
             _pauseMenu.SetActive(false);
             _controlsMenu.SetActive(false);
+
+            // SET BLUR EFFECT 
+            DepthOfField depthOfField;
+            if (_globalVolume.profile.TryGet<DepthOfField>(out depthOfField))
+                _depthOfField = depthOfField;
         }
 
         private void Update()
@@ -30,11 +43,15 @@ namespace Cursed.UI
                     _gameManager.State = GameManager.GameState.Pause;
                     _pauseMenu.SetActive(true);
                     _controlsMenu.SetActive(false);
+                    _inGameMenu.SetActive(false);
+                    _depthOfField.mode.value = DepthOfFieldMode.Gaussian;
                     break;
                 case GameManager.GameState.Pause:
                     _gameManager.State = GameManager.GameState.InGame;
                     _pauseMenu.SetActive(false);
                     _controlsMenu.SetActive(false);
+                    _inGameMenu.SetActive(true);
+                    _depthOfField.mode.value = DepthOfFieldMode.Off;
                     break;
                 case GameManager.GameState.InDevConsole:
                     break;
