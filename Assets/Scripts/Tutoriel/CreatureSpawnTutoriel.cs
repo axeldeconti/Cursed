1d6d90a18a0d1ed081364f19f858f980f68f4c51
Théo Gautier <36964591+WarZicks@users.noreply.github.com>
2020-05-04 16:19:05 +0200

    [Dev] Tuto - Spawn creature when player triggered

---
+++ Assets/Scripts/Tutoriel/CreatureSpawnTutoriel.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace Cursed.Tutoriel
+{
+    public class CreatureSpawnTutoriel : MonoBehaviour
+    {
+        [SerializeField] private GameObject _creature;
+
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            if (collision.gameObject.CompareTag("Player"))
+                Instantiate(_creature, transform.GetChild(0).position, Quaternion.identity);
+        }
+    }
+}
