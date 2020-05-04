b839be3a50e0e390a0c311426ee4094c186b016a
Théo Gautier <36964591+WarZicks@users.noreply.github.com>
2020-05-04 16:31:34 +0200

    [Int] Creature UI show in tuto

--- Assets/Scripts/Tutoriel/CreatureSpawnTutoriel.cs
+++ Assets/Scripts/Tutoriel/CreatureSpawnTutoriel.cs
@@ -5,11 +5,17 @@ namespace Cursed.Tutoriel
     public class CreatureSpawnTutoriel : MonoBehaviour
     {
         [SerializeField] private GameObject _creature;
+        [SerializeField] private GameObject _creatureLife;
+        [SerializeField] private GameObject _creatureStamina;
 
         private void OnTriggerEnter2D(Collider2D collision)
         {
             if (collision.gameObject.CompareTag("Player"))
+            {
                 Instantiate(_creature, transform.GetChild(0).position, Quaternion.identity);
+                _creatureLife.SetActive(true);
+                _creatureStamina.SetActive(true);
+            }
         }
     }
 }
