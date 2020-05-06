89c6db63ffb68edc41fc25e402253adbb2c47079
Théo Gautier <36964591+WarZicks@users.noreply.github.com>
2020-05-06 09:42:36 +0200

    [Dev] Black screen transitions scenes

---
+++ Assets/Scripts/UI/BlackScreenTransitions.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Cursed.UI
+{
+    public class BlackScreenTransitions : MonoBehaviour
+    {
+        private Animator _animator;
+
+        private void Awake()
+        {
+            _animator = GetComponent<Animator>();    
+        }
+
+        private void Start()
+        {
+            FadeOut();
+        }
+
+        public void FadeIn()
+        {
+            _animator.SetTrigger("FadeIn");
+        }
+
+        public void FadeOut()
+        {
+            _animator.SetTrigger("FadeOut");
+        }
+    }
+}
