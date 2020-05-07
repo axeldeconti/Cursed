d5c43b4595d3b3f8074df6fd97a0f548c23e17db
Théo Gautier <36964591+WarZicks@users.noreply.github.com>
2020-05-07 15:03:43 +0200

    [Dev] Clean cable door script + opti

--- Assets/Scripts/LevelBrics/InteractiveDoor.cs
+++ Assets/Scripts/LevelBrics/InteractiveDoor.cs
@@ -3,55 +3,25 @@
 public class InteractiveDoor : MonoBehaviour
 {
     [SerializeField] private bool _activeDoor = true;
-    [SerializeField] private GameObject _cablesLink;
-
-    [Header("Cable Color")]
-    [SerializeField] private Color _activeColor;
-    [SerializeField] private Color _deactiveColor;
-
     private Animator _animator;
-    private SpriteRenderer[] _cablesSprites;
+
+    public event System.Action<bool> doorToggle;
 
     private void Awake()
     {
         _animator = GetComponent<Animator>();
-
-        _cablesSprites = new SpriteRenderer[_cablesLink.transform.childCount];
-
-        for(int i = 0; i < _cablesLink.transform.childCount; i++)
-        {
-            _cablesSprites[i] = _cablesLink.transform.GetChild(i).GetComponent<SpriteRenderer>();
-        }
     }
 
     private void Start()
     {
         _animator.SetBool("Toggle", _activeDoor);
-        UpdateCablesColor();
+        doorToggle.Invoke(_activeDoor);
     }
 
     public void ToggleDoor()
     {
         _activeDoor = !_activeDoor;
         _animator.SetBool("Toggle", _activeDoor);
-        UpdateCablesColor();
-    }
-
-    private void UpdateCablesColor()
-    {
-        if(_activeDoor)
-        {
-            for(int i = 0; i < _cablesSprites.Length; i++)
-            {
-                _cablesSprites[i].color = _activeColor;
-            }
-        }
-        else
-        {
-            for (int i = 0; i < _cablesSprites.Length; i++)
-            {
-                _cablesSprites[i].color = _deactiveColor;
-            }
-        }
+        doorToggle.Invoke(_activeDoor);
     }
 }
