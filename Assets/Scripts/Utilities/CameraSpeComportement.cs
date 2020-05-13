6a1a20dc0e8641cf0a71c6297e1245e857f2efc8
Zookan <36925604+Zookan@users.noreply.github.com>
2020-05-13 11:14:22 +0200

    [Dev] Comportement camera : recentré sur l'avatar quand il est à l'arret

---
+++ Assets/Scripts/Utilities/CameraSpeComportement.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using Cinemachine;
+using Cursed.Character;
+
+namespace Cursed.Utilities
+{
+    public class CameraSpeComportement : MonoBehaviour
+    {
+        public static CameraSpeComportement Instance;
+
+        private CinemachineVirtualCamera _camera;
+        [SerializeField] private GameObject _refPlayer;
+        [SerializeField] private float _initialLookahead;
+
+        private void Awake()
+        {
+            Instance = this;
+            _camera = GetComponent<CinemachineVirtualCamera>();
+        }
+
+        private void Update()
+        {
+            //Camera Center if player stay in place
+            CameraCenter();
+        }
+
+        public void CameraCenter()
+        {
+            if (_refPlayer.GetComponent<CharacterMovement>().State == CharacterMovementState.Idle)
+                _camera.GetCinemachineComponent<CinemachineFramingTransposer>().m_LookaheadTime = 0f;
+            else
+                _camera.GetCinemachineComponent<CinemachineFramingTransposer>().m_LookaheadTime = _initialLookahead;
+        }
+    }
+}
