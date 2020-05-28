438e7e52db317cc84c5346641793d83bed0f289e
Théo Gautier <36964591+WarZicks@users.noreply.github.com>
2020-05-28 11:45:24 +0200

    [Update] Tilemap colors

---
+++ Assets/Editor/TileMapColor_Editor.cs
@@ -0,0 +1,21 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Cursed.LevelDesign
+{
+    [CustomEditor(typeof(TileMapColor))]
+    public class TileMapColor_Editor : Editor
+    {
+        private TileMapColor _tileMapColor = null;
+
+        private void OnEnable() => _tileMapColor = (TileMapColor)target;
+
+        public override void OnInspectorGUI()
+        {
+            if (GUILayout.Button("Update Tiles Color"))
+                _tileMapColor.UpdateTilesColor();
+
+            base.OnInspectorGUI();
+        }
+    }
+}
---
+++ Assets/Scripts/LevelDesign/TileMapColor.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace Cursed.LevelDesign
+{
+    public class TileMapColor : MonoBehaviour
+    {
+        [SerializeField] private Color _tileColor = new Color(.3f,.3f,.3f,1f);
+        private SpriteRenderer[] _tilesSprite;
+
+        public void UpdateTilesColor()
+        {
+            _tilesSprite = new SpriteRenderer[transform.childCount];
+            for(int i = 0; i < _tilesSprite.Length; i++)
+            {
+                _tilesSprite[i] = transform.GetChild(i).GetComponent<SpriteRenderer>();
+                _tilesSprite[i].color = _tileColor;
+            }
+        }
+    }
+}
