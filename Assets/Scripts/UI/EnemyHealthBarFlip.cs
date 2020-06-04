00d5ee75887c863183ff376c16a58d3961c8b2d5
Théo Gautier <36964591+WarZicks@users.noreply.github.com>
2020-06-04 12:17:34 +0200

    [Fix] Flip enemy health bar following enemy side

---
+++ Assets/Scripts/UI/EnemyHealthBarFlip.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using Cursed.Character;
+
+namespace Cursed.UI
+{
+    public class EnemyHealthBarFlip : MonoBehaviour
+    {
+        private CharacterMovement _characterMovementParent;
+        private RectTransform _rectTransform;
+        private float _initialXScale;
+
+        private void Awake()
+        {
+            _characterMovementParent = GetComponentInParent<CharacterMovement>();
+            _rectTransform = GetComponent<RectTransform>();
+            _initialXScale = _rectTransform.localScale.x;
+        }
+
+        private void Update()
+        {
+            UpdateScale();
+        }
+
+        private void UpdateScale()
+        {
+            if (_characterMovementParent == null)
+                return;
+
+            if(_characterMovementParent.Side == 1)
+                _rectTransform.localScale = new Vector3(_initialXScale, _rectTransform.localScale.y, _rectTransform.localScale.z);
+            else if(_characterMovementParent.Side == -1)
+                _rectTransform.localScale = new Vector3(-_initialXScale, _rectTransform.localScale.y, _rectTransform.localScale.z);
+        }
+    }
+}
+
