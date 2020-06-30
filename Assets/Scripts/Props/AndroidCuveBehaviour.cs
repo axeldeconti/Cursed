9b688c7e5014ffb3b8ab43f7e87bd1fa1aefc49c
Théo Gautier <36964591+WarZicks@users.noreply.github.com>
2020-06-30 15:38:53 +0200

    [Int] Add vibrations/cam shake to cuve broken

--- Assets/Scripts/Props/AndroidCuveBehaviour.cs
+++ Assets/Scripts/Props/AndroidCuveBehaviour.cs
@@ -1,7 +1,9 @@
-﻿using UnityEngine;
-using UnityEngine.SceneManagement;
-using Cursed.Managers;
+﻿using Cursed.Managers;
+using Cursed.Utilities;
+using Cursed.VisualEffect;
 using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace Cursed.Props
 {
@@ -12,9 +14,18 @@ namespace Cursed.Props
         [SerializeField] private RuntimeAnimatorController _brokingCuve;
         [SerializeField] private VoidEvent _cuveBroken;
 
+        [Header("Stats Vibration")]
+        [SerializeField] private VibrationEvent _onContrVibration = null;
+        [SerializeField] private VibrationData_SO _cuveBrokenVibration = null;
+
+        [Header("Stats Camera Shake")]
+        [SerializeField] private ShakeData _shakeCuveBroken = null;
+        [SerializeField] private ShakeDataEvent _onCamShake = null;
+
         private Animator _animator;
 
         private ControlerManager _controlerManager;
+        private bool _alreadyBroken = false;
 
         private void Start()
         {
@@ -29,7 +40,7 @@ namespace Cursed.Props
                 if (Input.GetButtonDown("Attack_1"))
                     CheckScene();
             }
-            else if(_controlerManager._ControlerType == ControlerManager.ControlerType.PS4)
+            else if (_controlerManager._ControlerType == ControlerManager.ControlerType.PS4)
             {
                 if (Input.GetButtonDown("Attack_1_PS4"))
                     CheckScene();
@@ -38,12 +49,17 @@ namespace Cursed.Props
 
         public void CheckScene()
         {
+            if (_alreadyBroken)
+                return;
+
             if (SceneManager.GetActiveScene().name == "Main")
             {
                 UpdateAnimator(_normalCuve);
+                _alreadyBroken = false;
             }
             else if (SceneManager.GetActiveScene().name == "Tuto" || SceneManager.GetActiveScene().name == "Intro")
             {
+                _alreadyBroken = true;
                 UpdateAnimator(_brokingCuve);
                 StartCoroutine(WaitForLaunchBrokenEffet(.25f));
             }
@@ -58,6 +74,11 @@ namespace Cursed.Props
         {
             yield return new WaitForSeconds(delay);
             UpdateAnimator(_brokenCuve);
+
+            // VIBRATIONS & CAM SHAKE
+            _onContrVibration?.Raise(_cuveBrokenVibration);
+            _onCamShake?.Raise(_shakeCuveBroken);
+
             _cuveBroken?.Raise();
             AkSoundEngine.PostEvent("Play_Cuve_Break", gameObject);
         }
